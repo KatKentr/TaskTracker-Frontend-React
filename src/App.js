3e30df2d73641b43fb07e1ae8e92fea5f7ef2ea6
Katerina <kkentroti@gmail.com>
2023-04-06 20:12:11 +0300

    app is running

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import LearningComponent from './components/learning-examples/LearningComponent.jsx';
+import TodoApp from './components/todo/TodoApp.jsx'
 
 function App() {
   return (
 
     //with className we specify the css class
     <div className="App">
-          My Todo Application updated
           
-          <LearningComponent  />
+          <TodoApp />
     </div>
   );
 }
---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,11 @@
+//the container for all the components that will be created
+export default function TodoApp(){
+
+    return (
+        <div className="TodoApp">
+            Todo Management Application     
+        </div>
+    )
+
+
+}
