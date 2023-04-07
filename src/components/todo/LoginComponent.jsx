41b98d1b3bd3711dd69436524470c09ab4b44455
Katerina <kkentroti@gmail.com>
2023-04-07 16:23:48 +0300

    hardcoded authentication added

--- src/components/todo/LoginComponent.jsx
+++ src/components/todo/LoginComponent.jsx
@@ -5,15 +5,67 @@ function LoginComponent() {
 
     // will return an array of two elements. when we invoke the value setUsername, we update the value stored in state
     const [username, setUsername]= useState('Katerina')
+    const [password, setPassword]= useState('')
+    const [showSuccessMessage, setShowSuccessMessage]= useState(false)
+    const [showErrorMessage, setShowErrorMessage]= useState(false)
+
 
     function handleUsernameChange(event){
         //console.log(event.target.value);
         setUsername(event.target.value);  //update the state
     }
 
+    function handlePasswordChange(event){
+        //console.log(event.target.value);
+        setPassword(event.target.value);  //update the state
+    }
+
+    function handleSubmit(event){
+
+        if (username==='Katerina' && password==='dummy'){
+
+            setShowSuccessMessage(true)
+            setShowErrorMessage(false)
+        } else {
+
+            setShowErrorMessage(true)
+            setShowSuccessMessage(false)
+
+        }
+    }
+
+
+        function SuccessMessageComponent(){
+
+            if ( showSuccessMessage){
+        
+                return (
+                    <div className="succeMessage">Authenticated Successfully</div>
+                )
+            }
+        
+            return null  
+        }
+
+        function ErrorMessageComponent(){
+
+            if (showErrorMessage){
+        
+                return (
+                    <div className="errorMessage">Authentication failed. Please check your credentials.</div>
+                )
+            }
+        
+            return null  
+        }
+
+
+
     return (
         <div className="Login">
-            Login Component
+            
+            <SuccessMessageComponent />
+            <ErrorMessageComponent />
             {/* username password button */}
             <div className="LoginForm">
 
@@ -24,15 +76,18 @@ function LoginComponent() {
                 </div>
                 <div>
                     <label>Password:</label>
-                    <input type="password" name="password" />
+                    <input type="password" value={password} name="password" onChange={handlePasswordChange}/>
                 </div>
                 <div>
-                    <button type="button" name="login" >login</button>
+                    <button type="button" name="login" onClick={handleSubmit}>login</button>
                 </div>
 
             </div>
         </div>
     )
 }
+
+
+
 
 export default LoginComponent;
