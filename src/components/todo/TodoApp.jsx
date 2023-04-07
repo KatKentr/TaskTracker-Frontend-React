357c3849b3e0bbc964e102f10761e07ab345c3f3
Katerina <kkentroti@gmail.com>
2023-04-07 17:10:19 +0300

    error component added for non-defined paths

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,6 +1,7 @@
 import './TodoApp.css'
 import LoginComponent from "./LoginComponent";
 import WelcomeComponent from "./WelcomeComponent";
+import ErrorComponent from './ErrorComponent';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 //the container for all the components that will be created
@@ -8,12 +9,12 @@ export default function TodoApp(){
 
     return (
         <div className="TodoApp">
-            Todo Management Application
             <BrowserRouter>
             <Routes>
                 <Route path='/' element={<LoginComponent />}></Route>
                 <Route path='/login' element={<LoginComponent />}></Route>
                 <Route path='/welcome' element={<WelcomeComponent />}></Route>
+                <Route path='*' element={<ErrorComponent />}></Route>   {/*error page is shown to the user for a not defined path */}
             </Routes>
             
             </BrowserRouter> 
