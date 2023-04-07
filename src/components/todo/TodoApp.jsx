a9077ea56ebcce31760a4b7a216b221e265790b7
Katerina <kkentroti@gmail.com>
2023-04-07 16:54:59 +0300

    routes added

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,15 +1,24 @@
 import './TodoApp.css'
 import LoginComponent from "./LoginComponent";
 import WelcomeComponent from "./WelcomeComponent";
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 //the container for all the components that will be created
 export default function TodoApp(){
 
     return (
         <div className="TodoApp">
-            Todo Management Application  
-            <LoginComponent />
-            {/* <WelcomeComponent />    */}
+            Todo Management Application
+            <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<LoginComponent />}></Route>
+                <Route path='/login' element={<LoginComponent />}></Route>
+                <Route path='/welcome' element={<WelcomeComponent />}></Route>
+            </Routes>
+            
+            </BrowserRouter> 
+            
+               
         </div>
     )
 
